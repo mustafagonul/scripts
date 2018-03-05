09388870aaaf5140645411b167f5ce4b062628fb
Mustafa Gonul <mustafagonul@gmail.com>
2018-03-05 09:55:53 +0300

    - vim-cpp.sh

---
+++ vim/vim-cpp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CURRENT_DIR= $(pwd)
+SESSION_NAME=$(basename ${CURRENT_DIR})
+
+
+# Create the session
+tmux new-session -s ${SESSION_NAME} -n vim-cpp -d
+
+
+# Split window
+tmux split-window -h -p 10
+
+
+# Select first pane (1) -- vim
+tmux select-pane -t 1
+tmux send-keys -t ${SESSION_NAME} 'vim' C-m
+
+
+# Attach session
+tmux attach -t ${SESSION_NAME}
+
