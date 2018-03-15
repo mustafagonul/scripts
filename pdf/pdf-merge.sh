32b5a40b2c4e70bca491bd02e6c5b1b290670727
Mustafa Gonul <mustafagonul@gmail.com>
2018-03-15 20:48:05 +0300

    - pdf merge script edited

--- pdf/pdf-merge.sh
+++ pdf/pdf-merge.sh
@@ -3,7 +3,8 @@
 # Reference:
 # https://stackoverflow.com/questions/2507766/merge-convert-multiple-pdf-files-into-one-pdf
 
-FILES=$(find . -name "*.pdf")
+# FILES=$(find . -name "*.pdf" -print0 | sort -z ) 
+FILES=$(ls *.pdf)
 
 if [[ -z "$FILES" ]]; then
 	echo "There is no pdf file!"
@@ -12,6 +13,7 @@ fi
 
 echo "Merging pdf files..."
 pdftk $FILES cat output output.pdf
+# echo ${FILES}
 
 echo "Done!"
 
