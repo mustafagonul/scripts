c9cf4585e71f3f499cc367170092a348098fbc43
Mustafa Gönül <mustafagonul@gmail.com>
2018-05-24 15:20:07 +0300

    - github-bitbake.sh corrected

--- github/github-bitbake.sh
+++ github/github-bitbake.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-set -x
+## Debug
+# set -x
 
+## Variables
 GITHUB_DIR=$HOME/git/GITHUB
 BITBAKE_DIR=$GITHUB_DIR/bitbake
 
-
-if [[ ! -d $BITBAKE_DIR ]]; then
-
-  pushd $GITHUB_DIR
-
-  git clone https://github.com/openembedded/bitbake/tree/master
-  git checkout 1.8.19
-
-  popd
-
+## Removes if the directory exists
+if [[ -d $BITBAKE_DIR ]]; then
+  rm -Rf $BITBAKE_DIR
 fi
+
+## Cloning bitbake
+pushd $GITHUB_DIR
+git clone https://github.com/openembedded/bitbake.git
+
+## Checking out 1.8.19
+pushd bitbake
+git checkout 1.8.19
+
+## Go back
+popd
+popd
+
